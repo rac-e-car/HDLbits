60fd73dad3c24a4a4d105287c89ea326ecc3491b
Siva S <siva2kshirpi@gmail.com>
2026-04-29 16:11:03 +0530

    Create wire.v

---
+++ Combinational_logic/basic_gates/wire.v
@@ -0,0 +1,12 @@
+/*Implement the following circuit:
+in ---------- out */
+
+module top_module (
+    input in,
+    output out);
+
+    always @(*) begin
+        out = in;
+    end
+    
+endmodule
