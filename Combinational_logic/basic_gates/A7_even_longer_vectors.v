e42d65461e8829941d6ee6e010023996ad22b0b0
Siva S <siva2kshirpi@gmail.com>
2026-05-02 21:30:11 +0530

    Create A7_even_longer_vectors.v

---
+++ Combinational_logic/basic_gates/A7_even_longer_vectors.v
@@ -0,0 +1,13 @@
+/* The same as the "A6_gates_and_vectors" program as before but with 100 bits */
+
+module top_module( 
+    input [99:0] in,
+    output [98:0] out_both,
+    output [99:1] out_any,
+    output [99:0] out_different );
+
+    assign out_both = in[99:1] & in[98:0];
+    assign out_any = in[99:1] | in[98:0];
+    assign out_different = in ^ {in[0], in[99:1]};
+    
+endmodule
