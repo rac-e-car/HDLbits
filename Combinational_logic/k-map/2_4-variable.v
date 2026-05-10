b92869cde76dc548d4f4cba09c320dd44e010e94
Siva S <siva2kshirpi@gmail.com>
2026-05-10 17:21:30 +0530

    Created 2_4-variable.v

---
+++ Combinational_logic/k-map/2_4-variable.v
@@ -0,0 +1,12 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  ); 
+    
+    assign out = ( (~a&~d) | (~c&~b) | (b&c&d) | (a&~b&d) );
+    
+    
+
+endmodule
