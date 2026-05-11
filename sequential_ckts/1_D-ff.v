6215512a535b2e6c2c38cc336b4f2c70493f08e8
Siva S <siva2kshirpi@gmail.com>
2026-05-11 09:18:47 +0530

    Created 1_D-ff.v

---
+++ sequential_ckts/1_D-ff.v
@@ -0,0 +1,17 @@
+/*A D flip-flop is a circuit that stores a bit and is updated periodically, 
+at the (usually) positive edge of a clock signal. Create a single D flip-flop. */
+
+module top_module (
+    input clk,    // Clocks are used in sequential circuits
+    input d,
+    output reg q );//
+
+    // Use a clocked always block
+    //   copy d to q at every positive edge of clk
+    //   Clocked always blocks should use non-blocking assignments
+    
+    always @(posedge clk) begin
+        q = d;
+    end
+endmodule
+
