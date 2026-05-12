03a65520263416ae1ba3cf5492942ab6187194de
Siva S <siva2kshirpi@gmail.com>
2026-05-12 13:53:32 +0530

    Created A8_Edge-detect_capture.v

---
+++ sequential_ckts/Latches&Flipflops/A8_Edge-detect_capture.v
@@ -0,0 +1,28 @@
+/*For each bit in a 32-bit vector, capture when the input signal changes from 1 in one clock cycle to 0 the next. 
+"Capture" means that the output will remain 1 until the register is reset (synchronous reset).
+
+Each output bit behaves like a SR flip-flop: The output bit should be set (to 1) the cycle after a 1 to 0 transition occurs. 
+The output bit should be reset (to 0) at the positive clock edge when reset is high. If both of the above events occur at the same time, 
+reset has precedence. In the last 4 cycles of the example waveform below, the 'reset' event occurs one cycle earlier than the 'set' event, 
+so there is no conflict here.*/
+
+module top_module (
+    input clk,
+    input reset,
+    input [31:0] in,
+    output [31:0] out
+);
+    reg [31:0] in_delay;
+    reg [31:0] d;
+    
+    always @(posedge clk) begin
+        in_delay <= in;
+        for(integer i=0; i<32; i=i+1) begin
+        if (reset) begin
+            out[i] <= 0;
+        end else if ( ~in[i] & in_delay[i]) begin
+            out[i] <= 1'b1;
+            end
+    end
+    end
+endmodule
