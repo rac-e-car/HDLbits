0ddc5b3e5ce2b2237098cbb9d41b64380e32ffaa
Siva S <siva2kshirpi@gmail.com>
2026-05-12 11:25:36 +0530

    Created A6_Edge_detect.v

---
+++ sequential_ckts/Latches&Flipflops/A6_Edge_detect.v
@@ -0,0 +1,14 @@
+/*For each bit in an 8-bit vector, detect when the input signal changes from 0 in one clock cycle 
+to 1 the next (similar to positive edge detection). The output bit should be set the cycle after a 0 to 1 transition occurs.*/
+
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);
+    reg [7:0] in_delay;
+    always @(posedge clk) begin
+       in_delay <= in;
+        pedge <= in & ~in_delay;
+    end
+endmodule
