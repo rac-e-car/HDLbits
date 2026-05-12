01d56271e9ed68c754fb5e0e9e5cfe7d4c977245
Siva S <siva2kshirpi@gmail.com>
2026-05-12 11:50:28 +0530

    Created A6_detect_both_edges.v

---
+++ sequential_ckts/Latches&Flipflops/A6_detect_both_edges.v
@@ -0,0 +1,15 @@
+/*For each bit in an 8-bit vector, detect when the input signal changes from one clock cycle 
+to the next (detect any edge). The output bit should be set the cycle after a 0 to 1 transition occurs. */
+
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    reg [7:0] in_delay;
+    
+    always @(posedge clk) begin
+        in_delay <= in;
+        anyedge <= in ^ in_delay;
+    end
+endmodule
