8d9a0156b69ed31665a0467285c4ca5946075839
Siva S <siva2kshirpi@gmail.com>
2026-05-13 18:44:19 +0530

    Created 7_shift_register.v

---
+++ sequential_ckts/shift_registers/7_shift_register.v
@@ -0,0 +1,17 @@
+/*Implement a 4 bit shift register that shifts right*/
+
+module top_module (
+    input clk,
+    input resetn,   // synchronous reset
+    input in,
+    output out);
+    reg [3:0] q;
+    
+    always @(posedge clk) begin
+        if (!resetn)
+            q <= 0;
+		else
+            q <= {in,q[3:1]};
+    end
+    assign out = q[0];
+endmodule
